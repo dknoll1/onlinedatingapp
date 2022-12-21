c8eebdca9095a625bbf3e105987c18eb25ce9da2
Daniel Knoll <knoll.daniel@student.greenriver.edu>
2022-12-21 22:06:36 +0000

    third commit for heroku

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = { 
+    MongoDB: 'mongodb+srv://odaadmin:D6eK03Ygy1uRyJfb@onlinedatingapp.1encedt.mongodb.net/?retryWrites=true&w=majority',
+    FacebookAppID: '622103609680741',
+    FacebookAppSecret: '53c4553ff90358f9c351027645053333',
+    GoogleID: '315120247018-g5r8mv9oqph50fprhc85ecdajmen8vc2.apps.googleusercontent.com',
+    GoogleSecret: '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'
+};
