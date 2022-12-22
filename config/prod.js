e09a2c0dae1b70741a3a5770ae96638660c9567a
Daniel Knoll <knoll.daniel@student.greenriver.edu>
2022-12-22 23:17:42 +0000

    facebook app updated

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports = { 
     MongoDB: process.env.MONGO_DB,
-    FacebookAppID: '622103609680741',
-    FacebookAppSecret: '53c4553ff90358f9c351027645053333',
+    FacebookAppID: '458258439849956',
+    FacebookAppSecret: '5a7ba412ae4b24e1550bc6f7188a44c3',
     GoogleID: '315120247018-g5r8mv9oqph50fprhc85ecdajmen8vc2.apps.googleusercontent.com',
     GoogleSecret: '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'
 };
