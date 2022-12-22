9eb0e65223d1e9ddbb30501a799996acc8c978b9
Daniel Knoll <knoll.daniel@student.greenriver.edu>
2022-12-22 23:09:25 +0000

    mongodb link updated

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = { 
-    MongoDB: 'mongodb+srv://odaadmin:D6eK03Ygy1uRyJfb@onlinedatingapp.1encedt.mongodb.net/?retryWrites=true&w=majority',
+    MongoDB: process.env.MONGO_DB,
     FacebookAppID: '622103609680741',
     FacebookAppSecret: '53c4553ff90358f9c351027645053333',
     GoogleID: '315120247018-g5r8mv9oqph50fprhc85ecdajmen8vc2.apps.googleusercontent.com',
