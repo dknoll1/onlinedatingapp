8fce639914501519b27df35980b16aa4b877b6da
Daniel Knoll <knoll.daniel@student.greenriver.edu>
2022-12-22 23:59:42 +0000

    hm

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports = { 
-    MongoDB: process.env.MONGO_DB,
-    FacebookAppID: '458258439849956',
-    FacebookAppSecret: '5a7ba412ae4b24e1550bc6f7188a44c3',
+    MongoDB: 'mongodb+srv://odaadmin:D6eK03Ygy1uRyJfb@onlinedatingapp.1encedt.mongodb.net/?retryWrites=true&w=majority',
+    FacebookAppID: process.env.FB_APP_ID,
+    FacebookAppSecret: process.env.FB_APP_SECRET,
     GoogleID: '315120247018-g5r8mv9oqph50fprhc85ecdajmen8vc2.apps.googleusercontent.com',
     GoogleSecret: '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'
 };
