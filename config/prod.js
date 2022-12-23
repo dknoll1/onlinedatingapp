aca331ba75c37147f9232f03d0a52a61f4d61b11
Daniel Knoll <knoll.daniel@student.greenriver.edu>
2022-12-23 16:54:43 +0000

    aws keys added

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = { 
+    MongoDB: process.env.MONGO_DB,
+    FacebookAppID: process.env.FB_APP_ID,
+    FacebookAppSecret: process.env.FB_APP_SECRET,
+    GoogleID: process.env.GOOGLE_ID,
+    GoogleSecret: process.env.GOOGLE_SECRET,
+    AWS_KEY: process.env.AWS_KEY,
+    AWS_SECRET: process.env.AWS_SECRET
+};
