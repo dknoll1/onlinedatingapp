c39776356ed3564e57733635ec4165d7fc711f0a
Daniel Knoll <knollmdan@gmail.com>
2022-12-22 16:19:23 -0800

    Delete prod.js

--- config/prod.js
+++
@@ -1,7 +0,0 @@
-module.exports = { 
-    MongoDB: 'mongodb+srv://odaadmin:D6eK03Ygy1uRyJfb@onlinedatingapp.1encedt.mongodb.net/?retryWrites=true&w=majority',
-    FacebookAppID: process.env.FB_APP_ID,
-    FacebookAppSecret: process.env.FB_APP_SECRET,
-    GoogleID: process.env.GOOGLE_ID,
-    GoogleSecret: process.env.GOOGLE_SECRET
-};
