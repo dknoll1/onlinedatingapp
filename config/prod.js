c6fb8fee86c70457133cef31c015ebb0a04dd247
Daniel Knoll <knoll.daniel@student.greenriver.edu>
2022-12-23 00:10:30 +0000

    google / mongodb

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@ module.exports = {
     MongoDB: 'mongodb+srv://odaadmin:D6eK03Ygy1uRyJfb@onlinedatingapp.1encedt.mongodb.net/?retryWrites=true&w=majority',
     FacebookAppID: process.env.FB_APP_ID,
     FacebookAppSecret: process.env.FB_APP_SECRET,
-    GoogleID: '315120247018-g5r8mv9oqph50fprhc85ecdajmen8vc2.apps.googleusercontent.com',
-    GoogleSecret: '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'
+    GoogleID: process.env.GOOGLE_ID,
+    GoogleSecret: process.env.GOOGLE_SECRET
 };
